ec12292ab9179b0de71314284592673e3ba73493
Tom Armitage <tom@infovore.org>
2014-04-16 16:51:55 +0100

    Capistrano 3.

---
+++ config/deploy/production.rb
@@ -0,0 +1,8 @@
+set :stage, :production
+set :rails_env, "production"
+
+set :branch, "master"
+set :deploy_to, "/var/www/borisbike_compass"
+
+server 'paprika.tomarmitage.com', user: 'twra2', roles: %w{app db}
+
