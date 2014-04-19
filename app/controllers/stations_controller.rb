d1d5c00a45e6e18514687b7520f0904fa0caf4a4
Tom Armitage <tom@infovore.org>
2014-04-19 11:32:10 +0100

    Include distance and arc from point to station when you get JSON.

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -11,6 +11,6 @@ class StationsController < ApplicationController
     hash = {:racks => nearest_with_empty_racks,
             :bikes => nearest_with_bikes}
 
-    render :json => hash.to_json(:methods => [:lat,:lon]) 
+    render :json => hash.to_json(:methods => [:lat,:lon, :distance, :arc]) 
   end
 end
