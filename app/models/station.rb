b042b1e7aec430ba7316a72981d65d7dc7145cde
Tom Armitage <tom@infovore.org>
2014-04-22 11:56:28 +0100

    Tidying.

--- app/models/station.rb
+++ app/models/station.rb
@@ -6,7 +6,6 @@ class Station < ActiveRecord::Base
   set_rgeo_factory_for_column(:latlon,
     RGeo::Geographic.spherical_factory(:srid => 4326))
 
-
   delegate :lat, :to => :latlon
   delegate :lon, :to => :latlon
 
